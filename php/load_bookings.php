ba6510ff53b06474cec16df2df1d2d899573b9b5
szlama94 <szlama94@gmail.com>
2025-02-18 17:10:20 +0100

    php formázás

--- php/load_bookings.php
+++ php/load_bookings.php
@@ -1,7 +1,6 @@
 <?php
 require_once('../../common/php/environment.php');
 
-// Adatbázis kapcsolat létrehozása
 $db = new Database();
 
 // A kérésből érkező paraméterek lekérése
@@ -9,17 +8,17 @@ $args = Util::getArgs();
 
 // SQL lekérdezés létrehozása, amely lekéri a felhasználó foglalásait
 // b = booking tábla s= services tábla
-$query = "SELECT 
-            b.id, 
-            DATE_FORMAT(b.booking_date, '%Y.%m.%d') AS date,  -- A foglalás dátumának formázása (év.hónap.nap)
-            TIME_FORMAT(b.booking_time, '%H:%i') AS time,  -- A foglalás időpontjának formázása (óra:perc)
-            s.services_name AS package,  -- A szolgáltatás neve
-            s.price  -- A szolgáltatás ára
-          FROM bookings b
-          INNER JOIN services s ON b.service_id = s.id  -- Kapcsolódás a services táblához a service_id alapján
-          WHERE b.user_id = ?  -- Csak a bejelentkezett felhasználó foglalásait kérdezi le
-          ORDER BY b.booking_date DESC, b.booking_time ASC";
-          //A foglalásokat dátum szerint csökkenő sorrendben rendezi, majd idő szerint növekvőben
+$query = "SELECT b.id, 
+          DATE_FORMAT(b.booking_date, '%Y.%m.%d') AS date, 
+          TIME_FORMAT(b.booking_time, '%H:%i') AS time, 
+                      s.services_name AS package,  
+                      s.price 
+          FROM        bookings b
+          INNER JOIN  services s 
+                  ON  b.service_id = s.id  
+               WHERE  b.user_id = ?  
+            ORDER BY  b.booking_date DESC, b.booking_time ASC";
+          
 
 // A lekérdezés végrehajtása és az eredmény visszaadása
 Util::setResponse($db->execute($query, [$args['user_id']]) ?: []);
